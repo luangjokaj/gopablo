48b394697a2c7017b3bf956b144a633b20883f00
Luan <luan@riangle.com>
2021-03-24 23:57:44 +0100

    👌 IMPROVE: Formatting

--- gulpfile.js
+++ gulpfile.js
@@ -4,27 +4,27 @@ GoPablo - Static site generator
 Contributors: Luan Gjokaj, Sherif Saleh
 
 -------------------------------------------------------------------------------------------------- */
-const { gulp, series, parallel, dest, src, watch } = require('gulp');
-const babel = require('gulp-babel');
-const browserSync = require('browser-sync').create();
-const concat = require('gulp-concat');
-const cssnano = require('cssnano');
-const del = require('del');
-const fileinclude = require('gulp-file-include');
-const gutil = require('gulp-util');
-const htmlmin = require('gulp-htmlmin');
-const imagemin = require('gulp-imagemin');
-const modRewrite = require('connect-modrewrite');
-const plumber = require('gulp-plumber');
-const postcss = require('gulp-postcss');
-const postcssImport = require('postcss-import');
-const postCSSMixins = require('postcss-mixins');
-const autoprefixer = require('autoprefixer');
-const postcssPresetEnv = require('postcss-preset-env');
-const RevAll = require('gulp-rev-all');
-const sourcemaps = require('gulp-sourcemaps');
-const uglify = require('gulp-uglify');
-const purgecss = require('gulp-purgecss');
+const { gulp, series, parallel, dest, src, watch } = require("gulp");
+const babel = require("gulp-babel");
+const browserSync = require("browser-sync").create();
+const concat = require("gulp-concat");
+const cssnano = require("cssnano");
+const del = require("del");
+const fileinclude = require("gulp-file-include");
+const gutil = require("gulp-util");
+const htmlmin = require("gulp-htmlmin");
+const imagemin = require("gulp-imagemin");
+const modRewrite = require("connect-modrewrite");
+const plumber = require("gulp-plumber");
+const postcss = require("gulp-postcss");
+const postcssImport = require("postcss-import");
+const postCSSMixins = require("postcss-mixins");
+const autoprefixer = require("autoprefixer");
+const postcssPresetEnv = require("postcss-preset-env");
+const RevAll = require("gulp-rev-all");
+const sourcemaps = require("gulp-sourcemaps");
+const uglify = require("gulp-uglify");
+const purgecss = require("gulp-purgecss");
 
 //--------------------------------------------------------------------------------------------------
 /* -------------------------------------------------------------------------------------------------
@@ -35,9 +35,9 @@ const pluginsDev = [
 	postcssPresetEnv({
 		stage: 0,
 		features: {
-			'nesting-rules': true,
-			'color-mod-function': true,
-			'custom-media': true,
+			"nesting-rules": true,
+			"color-mod-function": true,
+			"custom-media": true,
 		},
 	}),
 	postCSSMixins,
@@ -48,22 +48,22 @@ const pluginsProd = [
 	postcssPresetEnv({
 		stage: 0,
 		features: {
-			'nesting-rules': true,
-			'color-mod-function': true,
-			'custom-media': true,
+			"nesting-rules": true,
+			"color-mod-function": true,
+			"custom-media": true,
 		},
 	}),
 	postCSSMixins,
 	autoprefixer,
-	require('cssnano')(),
+	require("cssnano")(),
 ];
 
 //--------------------------------------------------------------------------------------------------
 /* -------------------------------------------------------------------------------------------------
 Header & Footer JavaScript Boundles
 -------------------------------------------------------------------------------------------------- */
-const headerJS = ['./node_modules/aos/dist/aos.js'];
-const footerJS = ['./node_modules/jquery/dist/jquery.js', './src/assets/js/**'];
+const headerJS = ["./node_modules/aos/dist/aos.js"];
+const footerJS = ["./node_modules/jquery/dist/jquery.js", "./src/assets/js/**"];
 
 //--------------------------------------------------------------------------------------------------
 /* -------------------------------------------------------------------------------------------------
@@ -71,18 +71,18 @@ Development Tasks
 -------------------------------------------------------------------------------------------------- */
 function devServer() {
 	browserSync.init({
-		logPrefix: '🐺 GoPablo',
+		logPrefix: "🐺 GoPablo",
 		server: {
-			baseDir: './build',
+			baseDir: "./build",
 		},
-		middleware: [modRewrite(['^.([^\\.]+)$ /$1.html [L]'])],
+		middleware: [modRewrite(["^.([^\\.]+)$ /$1.html [L]"])],
 	});
 
-	watch('./src/assets/css/**/*.css', stylesDev);
-	watch('./src/assets/js/**', series(footerScriptsDev, Reload));
-	watch('./src/assets/img/**', series(copyImagesDev, Reload));
-	watch('./src/assets/fonts/**', series(copyFontsDev, Reload));
-	watch('./src/**/*.html', series(stylesDev, staticFilesDev, Reload));
+	watch("./src/assets/css/**/*.css", stylesDev);
+	watch("./src/assets/js/**", series(footerScriptsDev, Reload));
+	watch("./src/assets/img/**", series(copyImagesDev, Reload));
+	watch("./src/assets/fonts/**", series(copyFontsDev, Reload));
+	watch("./src/**/*.html", series(stylesDev, staticFilesDev, Reload));
 }
 
 function Reload(done) {
@@ -91,30 +91,30 @@ function Reload(done) {
 }
 
 function copyImagesDev() {
-	return src('./src/assets/img/**').pipe(dest('./build/assets/img'));
+	return src("./src/assets/img/**").pipe(dest("./build/assets/img"));
 }
 
 function copyFontsDev() {
-	return src('./src/assets/fonts/**').pipe(dest('./build/assets/fonts'));
+	return src("./src/assets/fonts/**").pipe(dest("./build/assets/fonts"));
 }
 
 function stylesDev() {
-	return src('./src/assets/css/styles.css')
+	return src("./src/assets/css/styles.css")
 		.pipe(plumber({ errorHandler: onError }))
 		.pipe(sourcemaps.init())
 		.pipe(postcss(pluginsDev))
-		.pipe(sourcemaps.write('.'))
-		.pipe(dest('./build/assets/css'))
-		.pipe(browserSync.stream({ match: '**/*.css' }));
+		.pipe(sourcemaps.write("."))
+		.pipe(dest("./build/assets/css"))
+		.pipe(browserSync.stream({ match: "**/*.css" }));
 }
 
 function headerScriptsDev() {
 	return src(headerJS)
 		.pipe(plumber({ errorHandler: onError }))
 		.pipe(sourcemaps.init())
-		.pipe(concat('header-bundle.js'))
-		.pipe(sourcemaps.write('.'))
-		.pipe(dest('./build/assets/js'));
+		.pipe(concat("header-bundle.js"))
+		.pipe(sourcemaps.write("."))
+		.pipe(dest("./build/assets/js"));
 }
 
 function footerScriptsDev() {
@@ -123,26 +123,26 @@ function footerScriptsDev() {
 		.pipe(sourcemaps.init())
 		.pipe(
 			babel({
-				presets: ['@babel/preset-env'],
-			})
+				presets: ["@babel/preset-env"],
+			}),
 		)
-		.pipe(concat('footer-bundle.js'))
-		.pipe(sourcemaps.write('.'))
-		.pipe(dest('./build/assets/js'));
+		.pipe(concat("footer-bundle.js"))
+		.pipe(sourcemaps.write("."))
+		.pipe(dest("./build/assets/js"));
 }
 
 function staticFilesDev() {
-	return src('./src/*.html')
+	return src("./src/*.html")
 		.pipe(plumber({ errorHandler: onError }))
 		.pipe(
 			fileinclude({
 				filters: {
-					prefix: '@@',
-					basepath: '@file',
+					prefix: "@@",
+					basepath: "@file",
 				},
-			})
+			}),
 		)
-		.pipe(dest('./build'));
+		.pipe(dest("./build"));
 }
 
 exports.dev = series(
@@ -152,26 +152,26 @@ exports.dev = series(
 	headerScriptsDev,
 	footerScriptsDev,
 	staticFilesDev,
-	devServer
+	devServer,
 );
 
 /* -------------------------------------------------------------------------------------------------
 Production Tasks
 -------------------------------------------------------------------------------------------------- */
 async function cleanProd() {
-	await del(['./dist']);
+	await del(["./dist"]);
 }
 
 function copyFontsProd() {
-	return src('./src/assets/fonts/**').pipe(dest('./dist/assets/fonts'));
+	return src("./src/assets/fonts/**").pipe(dest("./dist/assets/fonts"));
 }
 
 function headerScriptsProd() {
 	return src(headerJS)
 		.pipe(plumber({ errorHandler: onError }))
-		.pipe(concat('header-bundle.js'))
+		.pipe(concat("header-bundle.js"))
 		.pipe(uglify())
-		.pipe(dest('./dist/assets/js'));
+		.pipe(dest("./dist/assets/js"));
 }
 
 function footerScriptsProd() {
@@ -179,84 +179,87 @@ function footerScriptsProd() {
 		.pipe(plumber({ errorHandler: onError }))
 		.pipe(
 			babel({
-				presets: ['@babel/preset-env'],
-			})
+				presets: ["@babel/preset-env"],
+			}),
 		)
-		.pipe(concat('footer-bundle.js'))
+		.pipe(concat("footer-bundle.js"))
 		.pipe(uglify())
-		.pipe(dest('./dist/assets/js'));
+		.pipe(dest("./dist/assets/js"));
 }
 
 function staticFilesProd() {
-	return src('./src/*.html')
+	return src("./src/*.html")
 		.pipe(plumber({ errorHandler: onError }))
 		.pipe(
 			fileinclude({
 				filters: {
-					prefix: '@@',
-					basepath: '@file',
+					prefix: "@@",
+					basepath: "@file",
 				},
-			})
+			}),
 		)
 		.pipe(
 			htmlmin({
 				collapseWhitespace: true,
-				ignoreCustomFragments: [/<%[\s\S]*?%>/, /<\?[=|php]?[\s\S]*?\?>/],
-			})
+				ignoreCustomFragments: [
+					/<%[\s\S]*?%>/,
+					/<\?[=|php]?[\s\S]*?\?>/,
+				],
+			}),
 		)
-		.pipe(dest('./dist'));
+		.pipe(dest("./dist"));
 }
 
 function copyImagesProd() {
-	return src('./src/assets/img/**').pipe(dest('./dist/assets/img'));
+	return src("./src/assets/img/**").pipe(dest("./dist/assets/img"));
 }
 
 function processImages() {
-	return src(['./dist/assets/img/**'])
+	return src(["./dist/assets/img/**"])
 		.pipe(plumber({ errorHandler: onError }))
 		.pipe(
 			imagemin([imagemin.svgo({ plugins: [{ removeViewBox: true }] })], {
 				verbose: true,
-			})
+			}),
 		)
-		.pipe(dest('./dist/assets/img'));
+		.pipe(dest("./dist/assets/img"));
 }
 
 function stylesProd() {
-	return src('./src/assets/css/styles.css')
+	return src("./src/assets/css/styles.css")
 		.pipe(plumber({ errorHandler: onError }))
 		.pipe(postcss(pluginsProd))
 		.pipe(
 			purgecss({
-				content: ['./src/**/*.html'],
-				whitelist: ['aos-animate'],
+				content: ["./src/**/*.html"],
+				whitelist: ["aos-animate"],
 				extractors: [
 					{
 						extractor: (content) => {
 							return content.match(/[A-z0-9-:\/]+/g) || [];
 						},
-						extensions: ['css', 'html'],
+						extensions: ["css", "html"],
 					},
 				],
-			})
+			}),
 		)
-		.pipe(dest('./dist/assets/css'));
+		.pipe(dest("./dist/assets/css"));
 }
 
 function copyEtcProd() {
-	return src('./src/etc/manifest.json').pipe(dest('./dist'));
+	return src("./src/etc/manifest.json").pipe(dest("./dist"));
 }
 
 function bustCaches() {
-	return src(['./dist/**'])
+	return src(["./dist/**"])
 		.pipe(
 			RevAll.revision({
-				dontRenameFile: [/^\/favicon.ico$/g, '.html'],
-				dontUpdateReference: [/^\/favicon.ico$/g, '.html'],
-			})
+				dontRenameFile: [/^\/favicon.ico$/g, ".html"],
+				dontUpdateReference: [/^\/favicon.ico$/g, ".html"],
+			}),
 		)
-		.pipe(dest('./dist'))
-		.on('end', () => {
+		.pipe(dest("./dist"))
+		.on("end", () => {
 			gutil.beep();
 			gutil.log(filesGenerated);
 			gutil.log(thankYou);
@@ -273,7 +276,7 @@ exports.prod = series(
 	processImages,
 	stylesProd,
 	copyEtcProd,
-	bustCaches
+	bustCaches,
 );
 
 /* -------------------------------------------------------------------------------------------------
@@ -281,20 +284,20 @@ Utility Tasks
 -------------------------------------------------------------------------------------------------- */
 const onError = (err) => {
 	gutil.beep();
-	gutil.log(staticBuild + ' - ' + errorMsg + ' ' + err.toString());
-	this.emit('end');
+	gutil.log(staticBuild + " - " + errorMsg + " " + err.toString());
+	this.emit("end");
 };
 
 /* -------------------------------------------------------------------------------------------------
 Messages
 -------------------------------------------------------------------------------------------------- */
-const errorMsg = '\x1b[41mError\x1b[0m';
+const errorMsg = "\x1b[41mError\x1b[0m";
 const filesGenerated =
-	'Your production file are generated in: \x1b[1m' + __dirname + '/dist/ ✅';
+	"Your production file are generated in: \x1b[1m" + __dirname + "/dist/ ✅";
 
-const staticBuild = '\x1b[42m\x1b[1m🐺 GoPablo\x1b[0m';
-const staticBuildUrl = '\x1b[2m - https://www.gopablo.co/\x1b[0m';
-const thankYou = 'Thank you for using ' + staticBuild + staticBuildUrl;
+const staticBuild = "\x1b[42m\x1b[1m🐺 GoPablo\x1b[0m";
+const staticBuildUrl = "\x1b[2m - https://www.gopablo.co/\x1b[0m";
+const thankYou = "Thank you for using " + staticBuild + staticBuildUrl;
 
 /* -------------------------------------------------------------------------------------------------
 End of all Tasks
